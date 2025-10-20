5dee831cf328da5ac733717cd75d29c38c7eaf5c
RoryNorbury <105914122@student.swin.edu.au>
2025-10-20 20:36:42 +1100

    CHanged point class to use doubles, and added a helper function for getting button coordinates

--- Core/Helpers.cs
+++ Core/Helpers.cs
@@ -1,10 +1,33 @@
-namespace Core;
+using SplashKitSDK;
 
-public class Point2i
+namespace Core;
+// stores a 2d floating point coordinate
+public class Point
 {
-    public int X;
-    public int Y;
-    public Point2i(int x, int y) { X = x; Y = y; }
-    public Point2i(Point2i pointt) { X = pointt.X; Y = pointt.Y; }
-    public Point2i(int[] point) { X = point[0]; Y = point[1]; }
+    public double X;
+    public double Y;
+    public Point(double x, double y) { X = x; Y = y; }
+    public Point(int x, int y) { X = x; Y = y; }
+    public Point(Point point) { X = point.X; Y = point.Y; }
+    public Point(double[] point) { X = point[0]; Y = point[1]; }
+    public static Point operator +(Point left, Point right) => new Point(left.X + right.X, left.Y + right.Y);
+    public static Point operator -(Point left, Point right) => new Point(left.X - right.X, left.Y - right.Y);
+    public static Point operator *(Point point, double scalar) => new Point(point.X * scalar, point.Y * scalar);
+    public static Point operator /(Point point, double scalar) => new Point(point.X / scalar, point.Y / scalar);
+    public static implicit operator Point2D(Point point) => new Point2D { X = point.X, Y = point.Y };
+}
+
+public class Helpers
+{
+    public Rectangle getMenuButtonRectangle(Point windowSize, int numButtons, int currentButton, double width, double height, double padding)
+    {
+        double y0 = 0.5 * (windowSize.Y - numButtons * height - (numButtons - 1) * padding);
+        Point p1 = new Point(
+            windowSize.X / 2.0 - width / 2.0,
+            y0 + currentButton * (height + padding)
+        );
+        Point p2 = new Point(p1);
+        p2 += new Point(width, height);
+        return SplashKit.RectangleFrom(p1, p2);
+    }
 }
