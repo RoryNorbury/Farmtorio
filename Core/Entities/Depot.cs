292b70ae960d087e7aa3c1bd4ba65ad61bc3cab7
RoryNorbury <105914122@student.swin.edu.au>
2025-11-05 17:15:13 +1100

    Implemented depot

--- Core/Entities/Depot.cs
+++ Core/Entities/Depot.cs
@@ -1,11 +1,45 @@
 namespace Core;
+using SplashKitSDK;
 
-public class Depot : Entity
+public class Depot : Entity, IHasInputSlots
 {
+    public override EntityID ID => EntityID.Depot;
+    public override bool isDirectional => false;
+    public int SellCount = 0;
+    public List<InventorySlot> InputSlots { get; } = [new InventorySlot()];
+    public Depot() : base() { }
+    public Depot(int sellCount, List<InventorySlot> inputSlots, Point2D position, int textureIndex) : base(position, OrientationID.North, textureIndex)
+    {
+        SellCount = sellCount;
+        InputSlots = inputSlots;
+    }
+    public Depot(int sellCount, List<InventorySlot> inputSlots, Entity baseEntity) : this(sellCount, inputSlots, baseEntity.Position, baseEntity.TextureIndex) { }
+    public override List<string> GetSaveData()
+    {
+        List<string> data = base.GetSaveData();
+        data.Add(SellCount.ToString());
+        return data;
+    }
+    public override void LoadFromData(List<string> data)
+    {
+        if (data.Count < 1)
+        {
+            throw new Exception($"Class 'Depot' requires at least 1 entry, {data.Count} provided.");
+        }
+        int i = data.Count - 1;
+        SellCount = int.Parse(data[i]);
+        data.RemoveAt(i);
+        base.LoadFromData(data);
+    }
     public override void Tick(double dt)
     {
         Ticked = true;
+
+        // sell all items in input slots (currently only one)
+        foreach (InventorySlot slot in InputSlots)
+        {
+            SellCount += slot.ItemCount;
+            slot.Clear();
+        }
     }
-    public override EntityID ID => EntityID.Depot;
-    public override bool isDirectional => false;
 }
