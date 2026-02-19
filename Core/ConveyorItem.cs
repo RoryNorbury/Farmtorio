7cf5ba30cec79f64fa91aa87319af01151e26a38
RoryNorbury <105914122@student.swin.edu.au>
2026-02-19 12:25:18 +1100

    Changed constructor to take double instead of float

--- Core/ConveyorItem.cs
+++ Core/ConveyorItem.cs
@@ -8,7 +8,7 @@ public class ConveyorItem
     public ItemID ItemID = 0;
     // distance along the conveyor in tiles
     public double Progress = 0;
-    public ConveyorItem(ItemID id, float progress)
+    public ConveyorItem(ItemID id, double progress)
     {
         ItemID = id;
         Progress = progress;
